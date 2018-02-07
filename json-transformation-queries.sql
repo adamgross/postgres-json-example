643da69b024677f4d235a9daf061b1b97bcfe400
Adam Gross <acg@heroku.com>
2018-02-07 13:37:15 -0800

    Update json-transformation-queries.sql

--- json-transformation-queries.sql
+++ json-transformation-queries.sql
@@ -1,4 +1,4 @@
-# Transform Google Vision API JSON to normalized Postgres SQL representation
+/* Transform Google Vision API JSON to normalized Postgres SQL representation */
 
 with q_a as (
     select 
@@ -12,3 +12,20 @@ q_b as (
 )
 
 select * from q_b, jsonb_to_record(q_b.color) as x(red text, blue int, green int) 
+
+/* Sort images based on color similarity 
+   Presumes the preceeding query results have been used to create a color_images table */
+
+WITH color_images as (
+    select id, row_number, red, green, blue from images_colors where id=2 order by score desc LIMIT 1
+)
+
+select image_colors.id, image_colors.row_number, images_sample_data.url,
+sqrt(
+    (image_colors.red::int - color_images.red::int)^2+
+    (image_colors.green::int - color_images.green::int)^2+
+    (image_colors.blue::int - color_images.blue::int)^2
+) as distance
+    from image_colors, color_images,images_sample_data
+    where image_colors.id = images_sample_data.id 
+    order by distance asc
