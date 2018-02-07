00d0b1d164f446158f4099d37dca3c09620b96b8
Adam Gross <acg@heroku.com>
2018-02-07 13:03:22 -0800

    Update json-transformation-queries.sql

--- json-transformation-queries.sql
+++ json-transformation-queries.sql
@@ -7,7 +7,8 @@ with q_a as (
 ),
 
 q_b as (
-    select color, score, "pixelFraction", q_a.id, ROW_NUMBER () OVER (PARTITION BY q_a.id ORDER BY "pixelFraction" desc) from q_a, jsonb_to_record(q_a.jarray) as x(color jsonb, score float, "pixelFraction" text)
+    select color, score, "pixelFraction", q_a.id, ROW_NUMBER () OVER (PARTITION BY q_a.id ORDER BY "pixelFraction" desc)
+    from q_a, jsonb_to_record(q_a.jarray) as x(color jsonb, score float, "pixelFraction" text)
 )
 
 select * from q_b, jsonb_to_record(q_b.color) as x(red text, blue int, green int) 
